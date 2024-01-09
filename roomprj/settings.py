7608335427bc45818229ef3cbe96b391ce130540
Korarak <irazor191119@gmail.com>
2024-01-09 22:22:03 +0700

    collectstaic fontawesome

--- roomprj/settings.py
+++ roomprj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
